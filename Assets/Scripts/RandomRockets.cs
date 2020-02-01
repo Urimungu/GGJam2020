0d11239fc2fbad14a9d60c74ed990fce7c8b31e8
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-02-01 15:09:47 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/RandomRockets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRockets : MonoBehaviour
+{
+
+    [SerializeField] private float missileFrequency;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject missile;
+    [SerializeField] private Transform target;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        target = player.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            Instantiate(missile, target.position, target.rotation);
+        }
+    }
+
+
+}
