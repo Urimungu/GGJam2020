80e7bbfd601f00338c000a033a38924d4197551f
Silas Reinagel <silas.reinagel@carvana.com>
2020-02-01 17:17:10 -0700

    Added Falling Sound Effect

--- Assets/Scripts/Audio/Events/PlayerEvents.cs
+++ Assets/Scripts/Audio/Events/PlayerEvents.cs
@@ -2,3 +2,4 @@ public sealed class PlayerStartedWalking {}
 public sealed class PlayerStoppedWalking {}
 public sealed class PlayerJumped {}
 public sealed class PlayerDoubleJumped {}
+public sealed class PlayerFalling {}
