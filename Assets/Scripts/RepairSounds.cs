43a049ada21c2a42d53480e29e6cf57a11ae7e1a
Ben <benbranc@uat.edu>
2020-02-01 18:49:36 -0700

    repair sound remake
    redo

---
+++ Assets/Scripts/RepairSounds.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class RepairSounds : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] repair;
+    [SerializeField] private float timeBetweenHits = 2f;
+
+    private float _cooldownRemaining;
+    private bool _isRepairing = true;
+    private int index;
+    private AudioSource repairHits;
+    
+
+    void Awake()
+    {
+        repairHits = GetComponent<AudioSource>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _cooldownRemaining = Mathf.Max(0, _cooldownRemaining - Time.deltaTime);
+        if (!_isRepairing || _cooldownRemaining > 0)
+            return;
+
+        _cooldownRemaining = timeBetweenHits;
+        index = (index + 1) % repair.Length;
+        var repairSound = repair[index];
+        repairHits.PlayOneShot(repairSound);
+        Debug.Log("FootStep");
+    }
+}
