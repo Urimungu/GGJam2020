692088195860b87f2ef5e6b96f25ba95aae82040
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-02-01 21:58:30 -0700

    Missile Frequency Added
    Missiles attack at random. Not in main level

---
+++ Assets/MissileLaunch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLaunch : MonoBehaviour
+{
+    private Vector3 direction;
+    private Vector3 target;
+    public GameObject player;
+    public float speed = 2f;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameManager.Manager.Player;
+        target = player.transform.position;
+        direction = Vector3.MoveTowards(transform.position, target, speed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Rocket sees " + player);
+        transform.position = transform.position + direction;
+        //if(transform.position == target)
+        //{
+        //    Destroy(gameObject);
+        //}
+
+    }
+}
---
+++ Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMe : MonoBehaviour
+{
+    [SerializeField] private float timer = 1f;
+    private float createTime = 0.0f;
+
+    private void Start()
+    {
+        createTime = Time.time;
+    }
+    private void Update()
+    {
+
+        if ((Time.time - createTime) > timer)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
