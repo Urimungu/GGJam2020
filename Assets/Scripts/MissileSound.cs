a0629c8307d9fc9ddbddc1690ebdde28ed517d5a
Ben <benbranc@uat.edu>
2020-02-02 13:50:07 -0700

    missile launch volume
    update

--- Assets/Scripts/MissileSound.cs
+++ Assets/Scripts/MissileSound.cs
@@ -6,15 +6,17 @@ public class MissileSound : OnMessage<MissileDetonated>
     [SerializeField] private UiSfxPlayer uiSfx;
     [SerializeField] private AudioClip launch;
     [SerializeField] private AudioClip explode;
-   
+    [SerializeField] private float launchVolume;
+
     private AudioSource audioSource;
+   
 
 
     protected override void Execute(MissileDetonated msg) => uiSfx.Play(explode);
     void Awake() => audioSource = GetComponent<AudioSource>();
     private void Start()
     {
-            audioSource.PlayOneShot(launch);
+            audioSource.PlayOneShot(launch, launchVolume);
 }
 
 }
