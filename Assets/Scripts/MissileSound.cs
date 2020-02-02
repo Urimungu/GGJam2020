2df186349d914ef49b2a2c18efb8f129f8ce5533
Ben <benbranc@uat.edu>
2020-02-02 13:10:47 -0700

    missile Sound
    missile sound

---
+++ Assets/Scripts/MissileSound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MissileSound : OnMessage<MissileLaunched,MissileDetonated>
+{
+    [SerializeField] private AudioClip launch;
+    [SerializeField] private AudioClip explode;
+   
+    private AudioSource audioSource;
+
+    protected override void Execute(MissileLaunched msg) => audioSource.PlayOneShot(launch);
+    protected override void Execute(MissileDetonated msg) => audioSource.PlayOneShot(explode);
+    void Awake() => audioSource = GetComponent<AudioSource>();
+    
+}
