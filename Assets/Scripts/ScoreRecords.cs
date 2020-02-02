404d37631f246f3f00979f850806966d5322e4d8
Elijah <enutter24@gmail.com>
2020-02-02 14:22:11 -0700

    LeaderBoard/Score

---
+++ Assets/Scripts/ScoreRecords.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScoreRecords : MonoBehaviour
+{
+    /*This will keep track of all scores recorded.
+     So if you pull up from Start Screen, it can load all recent scores that
+     has been set.
+     
+     From SinglePlayer, if you die, Score.cs should load a new score to a file, 
+     organ the scores in order, and update what ScoreRecords read and display on the
+     screen.*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AddNewScore(int _value)
+    {
+
+    }
+}
